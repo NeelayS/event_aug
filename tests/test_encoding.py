a56edf02040152521c7aadebdf7002fde3104dcb
NeelayS <shahnh19@gmail.com>
2022-06-19 01:25:17 +0530

    1. Added delta intensity spike encoding function 2. Renamed file containing spike encoding functions

--- tests/test_encoding.py
+++ tests/test_encoding.py
@@ -1,23 +1,43 @@
 import numpy as np
 import torch
 
-from event_aug.encoding import rate_code
+from event_aug.spike_encoding import delta_intensity_code, rate_code
 
 
 def test_rate_code():
 
-    arr_n = np.array([[1, 2], [3, 4]])
-    out = rate_code(arr_n)
+    data_n = np.array([[1, 2], [3, 4]])
+    data_t = torch.from_numpy(data_n)
+
+    out = rate_code(data_n)
     assert out.shape == (2, 2)
 
-    arr_t = torch.Tensor([[1, 2], [3, 4]])
-    out = rate_code(arr_t, n_steps=2)
+    out = rate_code(data_t, n_steps=2)
     assert out.shape == (2, 2, 2)
 
-    arr = np.stack([arr_n, arr_n], axis=0)
-    out = rate_code(arr, gain=0.1)
+    data = np.stack([data_n, data_n], axis=0)
+    out = rate_code(data, gain=0.1)
     assert out.shape == (2, 2, 2)
 
-    arr = torch.stack([arr_t, arr_t], axis=0)
-    out = rate_code(arr, n_steps=2, gain=0.3)
+    data = torch.stack([data_t, data_t], axis=0)
+    out = rate_code(data, n_steps=2, gain=0.3)
     assert out.shape == (2, 2, 2)
+
+
+def test_delta_intensity_code():
+
+    data_t = (torch.randn(10, 32, 32) * 255).type(torch.torch.LongTensor)
+
+    out = delta_intensity_code(data_t, threshold=25)
+    assert out.shape == (10, 32, 32)
+
+    out = delta_intensity_code(data_t, threshold=25, use_negative_delta=False)
+    assert out.shape == (10, 32, 32)
+
+    out = delta_intensity_code(data_t, threshold=25, ignore_start=True)
+    assert out.shape == (9, 32, 32)
+
+    out = delta_intensity_code(
+        data_t.numpy(), threshold=25, ignore_start=True, use_negative_delta=False
+    )
+    assert out.shape == (9, 32, 32)
