f8c3d56e015dcd4dde2b867d794e5950ae8f74b9
Neelay Shah <shahnh19@gmail.com>
2022-06-21 23:14:42 +0530

    Add spike encoding for videos based on changes in intensities (#15)
    * 1. Added delta intensity spike encoding function
    2. Renamed file containing spike encoding functions
    
    * Renamed test file for spike encoding functions
    
    * Minor
    
    * Renamed a function argument
    
    * Modified spike encoding function to work with multi-channel input as well
    
    * Removed unused package from dependencies
    
    * Added test for multi-channel input
    
    * Formatting

---
+++ event_aug/spike_encoding.py
@@ -0,0 +1,102 @@
+from typing import Union
+
+import numpy as np
+import torch
+
+
+def rate_code(
+    data: Union[np.ndarray, torch.Tensor], n_steps: int = 1, gain: int = 1
+) -> np.ndarray:
+
+    """
+    Converts array of continuous input data to spikes using rate coding.
+    Input array is normalized to [0, 1] and the resulting elements are treated as
+    probabilities for Bernoulli trials.
+
+    Parameters
+    ----------
+    data: np.ndarray or torch.Tensor
+        2D array of continuous input data. 3D array in case of time-series data (shape: T x H x W).
+    n_steps: int
+        Number of time-steps to perform rate coding for. Only used for 2D input data.
+        In case of time series data, rate coding is performed for each time-step.
+    gain: int
+        Factor by which to scale normalized input features.
+
+    Returns
+    -------
+    np.ndarray
+        2D array of spikes. 3D array in case of time-series data.
+    """
+
+    data = data / np.linalg.norm(data)
+    data = data * gain
+
+    if not isinstance(data, torch.Tensor):
+        data = torch.from_numpy(data).float()
+
+    assert (
+        len(data.shape) == 2 or len(data.shape) == 3
+    ), "Input must be a 2D array of probabilities or a 3D array in case of time series data"
+
+    if len(data.shape) == 2 and n_steps > 1:
+        new_data = torch.clone(data)
+        for _ in range(n_steps - 1):
+            new_data = torch.stack((new_data, data))
+        data = new_data
+
+    spikes = torch.bernoulli(data)
+
+    return spikes.numpy()
+
+
+def delta_intensity_code(
+    data: Union[np.ndarray, torch.Tensor],
+    threshold: int,
+    use_negative_delta=True,
+    exclude_start=False,
+) -> Union[np.ndarray, torch.Tensor]:
+
+    """
+    Converts video data to spikes. If the difference in the intensity of a pixel in
+    consecutive frames is greater than the threshold, the pixel is assigned an event.
+
+    Parameters
+    ----------
+    data: np.ndarray or torch.Tensor
+        Array containing the video data (frames). Should be of shape (T x H x W) or (T x H x W x C).
+    threshold: int
+        Threshold for the difference in intensities of pixels in consecutive frames
+        for assigning an event.
+    use_negative_delta: bool
+        Whether to consider decreases in intensity as well along with increases.
+    exclude_start: bool
+        Whether to not return the spikes for the first frame which will always be 0 for all pixels.
+
+    Returns
+    -------
+    np.ndarray or torch.Tensor
+        Array containing the spikes.
+    """
+
+    assert (
+        len(data.shape) == 3 or len(data.shape) == 4
+    ), "Input must be an array with shape (T x H x W) or (T x H x W x C)"
+
+    if isinstance(data, torch.Tensor):
+        spikes = torch.zeros_like(data)
+    else:
+        spikes = np.zeros_like(data)
+
+    for i in range(1, data.shape[0]):
+
+        intensity_delta = data[i] - data[i - 1]
+        if use_negative_delta:
+            intensity_delta = abs(intensity_delta)
+
+        spikes[i][intensity_delta > threshold] = 1
+
+    if exclude_start:
+        return spikes[1:]
+
+    return spikes
---
+++ tests/test_spike_encoding.py
@@ -0,0 +1,48 @@
+import numpy as np
+import torch
+
+from event_aug.spike_encoding import delta_intensity_code, rate_code
+
+
+def test_rate_code():
+
+    data_n = np.array([[1, 2], [3, 4]])
+    data_t = torch.from_numpy(data_n)
+
+    out = rate_code(data_n)
+    assert out.shape == (2, 2)
+
+    out = rate_code(data_t, n_steps=2)
+    assert out.shape == (2, 2, 2)
+
+    data = np.stack([data_n, data_n], axis=0)
+    out = rate_code(data, gain=0.1)
+    assert out.shape == (2, 2, 2)
+
+    data = torch.stack([data_t, data_t], axis=0)
+    out = rate_code(data, n_steps=2, gain=0.3)
+    assert out.shape == (2, 2, 2)
+
+
+def test_delta_intensity_code():
+
+    data_t = (torch.rand(10, 32, 32) * 255).type(torch.LongTensor)
+
+    out = delta_intensity_code(data_t, threshold=25)
+    assert out.shape == (10, 32, 32)
+
+    out = delta_intensity_code(data_t, threshold=25, use_negative_delta=False)
+    assert out.shape == (10, 32, 32)
+
+    out = delta_intensity_code(data_t, threshold=25, exclude_start=True)
+    assert out.shape == (9, 32, 32)
+
+    out = delta_intensity_code(
+        data_t.numpy(), threshold=25, exclude_start=True, use_negative_delta=False
+    )
+    assert out.shape == (9, 32, 32)
+
+    data_t = (torch.rand(10, 32, 32, 3) * 255).type(torch.LongTensor)
+
+    out = delta_intensity_code(data_t, threshold=25)
+    assert out.shape == (10, 32, 32, 3)
